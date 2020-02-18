3096cde630b7d7958af195ed3a9d7753d8978846
Kate McKinney <kate.mckinney210@gmail.com>
2020-02-18 11:00:12 -0600

    daily exercises

---
+++ js/circle.js
@@ -0,0 +1,50 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+            var area = (Math.PI * Math.pow(circle.radius, 2));
+
+            // TODO: return the proper value
+            return area;
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+            if (doRounding === true) {
+                console.log("When r = " + this.radius + ", Area = " +                 Math.round(this.getArea()));
+            }
+            else{
+                return console.log("When r = " + this.radius + ", Area = " + this.getArea());
+            }
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information:");
+    circle.logInfo(false);
+    console.log("Rounded to the nearest whole number:");
+    circle.logInfo(true);
+
+
+    console.log("=======================================");
+
+
+    // TODO: Change the radius of the circle to 5.
+
+     circle.radius = 5;
+
+    // log info about the circle
+    console.log("Raw circle information:");
+    circle.logInfo(false);
+    console.log("Rounded to the nearest whole number:");
+    circle.logInfo(true);
+})();
+
